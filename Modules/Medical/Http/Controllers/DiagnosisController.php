b439f61b692d0f20bbcc6ae7b366d70a1fb38068
abdulrhman-developer0 <tknwtknwlwjy10@gmail.com>
2024-10-12 14:10:34 +0300

    test

--- Modules/Medical/Http/Controllers/DiagnosisController.php
+++ Modules/Medical/Http/Controllers/DiagnosisController.php
@@ -98,7 +98,7 @@ class DiagnosisController extends Controller
         if ($appointment->visit_type == 'examination') {
 
             // The name of service.
-            dd($appointment->doctor->speciality);
+            dd($appointment->doctor->speciality_id);
             $name = $appointment->doctor?->speciality?->name;;
 
             // The examination coset of doctor speciality.
